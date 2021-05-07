2706469820105793496ff968ae43b957b3cb61de
Артем <artem-solovyev@mail.ru>
2021-05-07 16:30:22 +0300

    bugs fixed first

---
+++ backend/src/main/java/com/aviaticket/backend/rest/UserRestController.java
@@ -0,0 +1,34 @@
+package com.aviaticket.backend.rest;
+
+import com.aviaticket.backend.dto.UserDto;
+import com.aviaticket.backend.models.User;
+import com.aviaticket.backend.service.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDto>> listPosts() {
+        List<UserDto> usersDtos = userService.listAll();
+        if (usersDtos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(usersDtos, HttpStatus.OK);
+
+    }
+}
