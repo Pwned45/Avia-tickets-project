71b54611c32d5b2971b61af441570adfa6d34248
Pwned45 <daniloid2015@yandex.ru>
2021-06-13 22:35:18 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+// import {authInterceptorProviders} from './AuthInterceptor/AuthInterceptor';
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HomepageComponent} from "./homepage/homepage.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
