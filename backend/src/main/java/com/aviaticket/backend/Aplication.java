c467ff0ebf63776be4124c152bdcd24dbb2125a8
Артем <artem-solovyev@mail.ru>
2021-06-15 00:07:35 +0300

    jar

--- backend/src/main/java/com/aviaticket/backend/Aplication.java
+++ backend/src/main/java/com/aviaticket/backend/Aplication.java
@@ -2,10 +2,10 @@ package com.aviaticket.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class Aplication {
     public static void main(String[] args) {
         SpringApplication.run(Aplication.class, args);
