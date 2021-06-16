5b6ea623579745057ef1a3b20273f750518fb11f
Артем <artem-solovyev@mail.ru>
2021-06-16 20:23:43 +0300

    some change

--- backend/target/generated-sources/annotations/com/aviaticket/backend/convector/LocationMapperImpl.java
+++ backend/target/generated-sources/annotations/com/aviaticket/backend/convector/LocationMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-15T18:05:32+0300",
+    date = "2021-06-16T20:22:15+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_181 (Oracle Corporation)"
 )
 @Component
