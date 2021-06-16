ff3fe0ebbc6690c08f1aed3c98cae7f9abd22a20
Pwned45 <daniloid2015@yandex.ru>
2021-06-16 12:39:44 +0300

    profile admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {RegisrtComponent} from "./regisrt/regisrt.component";
 import {LoginComponent} from "./login/login.component";
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from "@angular/common/http";
+import {AdminComponent} from "./admin/admin.component";
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import {HttpClientModule} from "@angular/common/http";
     ProfileComponent,
     RegisrtComponent,
     LoginComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
