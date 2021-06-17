9ba25b872ba7305f856ef4c4ac34b6b7d89ac4a7
Артем <artem-solovyev@mail.ru>
2021-06-17 11:13:52 +0300

    v23

--- backend/src/main/java/com/aviaticket/backend/service/sevimpl/UserServiceImpl.java
+++ backend/src/main/java/com/aviaticket/backend/service/sevimpl/UserServiceImpl.java
@@ -23,17 +23,17 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final ChecksMapper checksMapper;
-    private final BCryptPasswordEncoder passwordEncoder;
+//    private final BCryptPasswordEncoder passwordEncoder;
     private final LocationRepository locationRepository;
     private final TicketMapper ticketMapper;
     private final ConditionalsMapper conditionalsMapper;
 
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, ChecksMapper checksMapper, BCryptPasswordEncoder passwordEncoder, LocationRepository locationRepository, TicketMapper ticketMapper, ConditionalsMapper conditionalsMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, ChecksMapper checksMapper, LocationRepository locationRepository, TicketMapper ticketMapper, ConditionalsMapper conditionalsMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.checksMapper = checksMapper;
-        this.passwordEncoder = passwordEncoder;
+//        this.passwordEncoder = passwordEncoder;
         this.locationRepository = locationRepository;
         this.ticketMapper = ticketMapper;
         this.conditionalsMapper = conditionalsMapper;
@@ -96,9 +96,9 @@ public class UserServiceImpl implements UserService {
         newUser.setLocation(locationRepository.findById(userDto.getLocationDto().getIdLocation()).orElseThrow(EntityNotFoundException::new));
         newUser.setName(userDto.getName());
         newUser.setLastName(userDto.getLastName());
-        if (userDto.getPass() != null) {
-            newUser.setPass(passwordEncoder.encode(userDto.getPass()));
-        }
+//        if (userDto.getPass() != null) {
+//            newUser.setPass(passwordEncoder.encode(userDto.getPass()));
+//        }
         newUser.setBornDay(userDto.getBornDay());
         newUser.setPhone(userDto.getPhone());
         userRepository.save(newUser);
@@ -120,7 +120,7 @@ public class UserServiceImpl implements UserService {
         user.setPhone(userDto.getPhone());
         user.setLogin(userDto.getLogin());
         user.setEmail(userDto.getEmail());
-        user.setPass(passwordEncoder.encode(userDto.getPass()));
+//        user.setPass(passwordEncoder.encode(userDto.getPass()));
         user.setRoles(Roles.USER);
         userRepository.save(user);
 
