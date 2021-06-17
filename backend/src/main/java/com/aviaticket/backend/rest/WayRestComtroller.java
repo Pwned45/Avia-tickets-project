a0fd076db4664d4fd759b452c31bca6abaeb8b2b
Артем <artem-solovyev@mail.ru>
2021-06-17 22:18:16 +0300

    v25

--- backend/src/main/java/com/aviaticket/backend/rest/WayRestComtroller.java
+++ backend/src/main/java/com/aviaticket/backend/rest/WayRestComtroller.java
@@ -1,30 +1,30 @@
-//package com.aviaticket.backend.rest;
-//import com.aviaticket.backend.dto.WayDto;
-//import com.aviaticket.backend.service.WayServise;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/way")
-//public class WayRestComtroller {
-//    @Autowired
-//    private WayServise wayServise;
-//
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping("/all")
-//    public ResponseEntity<List<WayDto>> allUsers() {
-//        List<WayDto> wayDtos = wayServise.getAvailWay();
-//        if (wayDtos.isEmpty()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        return new ResponseEntity<>(wayDtos, HttpStatus.OK);
-//    }
-//
-//}
+package com.aviaticket.backend.rest;
+import com.aviaticket.backend.dto.WayDto;
+import com.aviaticket.backend.service.WayServise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/way")
+public class WayRestComtroller {
+    @Autowired
+    private WayServise wayServise;
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/all")
+    public ResponseEntity<List<WayDto>> allUsers() {
+        List<WayDto> wayDtos = wayServise.getAvailWay();
+        if (wayDtos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(wayDtos, HttpStatus.OK);
+    }
+
+}
--- backend/src/main/java/com/aviaticket/backend/service/WayServise.java
+++ backend/src/main/java/com/aviaticket/backend/service/WayServise.java
@@ -4,5 +4,5 @@ import com.aviaticket.backend.dto.WayDto;
 import java.util.List;
 
 public interface WayServise {
-//    List<WayDto> getAvailWay();
+    List<WayDto> getAvailWay();
 }
