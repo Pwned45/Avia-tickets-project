84cbbcd1cac4057181554a2b9336ac29cd8c6891
Артем <artem-solovyev@mail.ru>
2021-06-17 22:15:12 +0300

    v25

--- backend/target/generated-sources/annotations/com/aviaticket/backend/convector/UserMapperImpl.java
+++ backend/target/generated-sources/annotations/com/aviaticket/backend/convector/UserMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-17T22:10:48+0300",
+    date = "2021-06-17T22:14:48+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_181 (Oracle Corporation)"
 )
 @Component
